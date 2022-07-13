265ebcec466cd5dafa00c7123c765c521ff29f06
IvanHdzF <ivan083198@hotmail.com>
2022-07-12 21:13:09 -0700

    Update build-guisim.sh

--- scripts/build-guisim.sh
+++ scripts/build-guisim.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # notice how we avoid spaces in $now to avoid quotation hell in go build command
+sudo apt-get install gcc-multilib
+sudo apt-get install gcc-mingw-w64
 now=$(date --utc +%FT%T%Z)
-GOOS=windows GOARCH=amd64 go build -o guisim.exe -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now" ./cmd/guisim/ 
+GOOS=windows GOARCH=386 \
+  CGO_ENABLED=1 CXX=i686-w64-mingw32-g++ CC=i686-w64-mingw32-gcc \
+  go build -o guisim.exe -ldflags "-X main.sha1ver=`git rev-parse HEAD` -X main.buildTime=$now" ./cmd/guisim/ 
 
 # ls
